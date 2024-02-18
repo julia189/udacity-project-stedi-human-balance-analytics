57ea3d11f9dc32564b96d5d2c997208ca4357b9c
Julia Hehl <julia.hehl@bmw.de>
2024-02-18 16:24:15 +0100

    Addded script

--- machine_learning_curated.py
+++ machine_learning_curated.py
@@ -13,17 +13,17 @@ job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
 # Script generated for node Step Trainer Trusted Zone
-StepTrainerTrustedZone_node1679503421946 = (
+StepTrainerTrustedZone_node1689516451947 = (
     glueContext.create_dynamic_frame.from_catalog(
-        database="stedi-lakehouse",
+        database="stedi_database",
         table_name="step_trainer_trusted",
-        transformation_ctx="StepTrainerTrustedZone_node1679503421946",
+        transformation_ctx="StepTrainerTrustedZone_node1689516451947",
     )
 )
 
 # Script generated for node Accelerometer Trusted Zone
 AccelerometerTrustedZone_node1 = glueContext.create_dynamic_frame.from_catalog(
-    database="stedi-lakehouse",
+    database="stedi_database",
     table_name="accelerometer_trusted",
     transformation_ctx="AccelerometerTrustedZone_node1",
 )
@@ -31,7 +31,7 @@ AccelerometerTrustedZone_node1 = glueContext.create_dynamic_frame.from_catalog(
 # Script generated for node ApplyJoinMapping
 ApplyJoinMapping_node2 = Join.apply(
     frame1=AccelerometerTrustedZone_node1,
-    frame2=StepTrainerTrustedZone_node1679503421946,
+    frame2=StepTrainerTrustedZone_node1689516451947,
     keys1=["timestamp"],
     keys2=["sensorreadingtime"],
     transformation_ctx="ApplyJoinMapping_node2",
@@ -41,13 +41,13 @@ ApplyJoinMapping_node2 = Join.apply(
 DropFields_node1679503544811 = DropFields.apply(
     frame=ApplyJoinMapping_node2,
     paths=["user"],
-    transformation_ctx="DropFields_node1679503544811",
+    transformation_ctx="DropFields_node1679504545812",
 )
 
 # Script generated for node Step Trainer Curated Zone
 StepTrainerCuratedZone_node3 = glueContext.write_dynamic_frame.from_catalog(
     frame=DropFields_node1679503544811,
-    database="stedi-lakehouse",
+    database="stedi_database",
     table_name="machine_learning_curated",
     transformation_ctx="StepTrainerCuratedZone_node3",
 )
